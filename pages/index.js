2f22836b83d3739aaae05ce75def91db49ac2cf7
jeremy <jpwijanto@gmail.com>
2022-12-02 14:34:54 +0700

    fix gtag

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,12 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <script>{`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MSH8C97');
-          `}</script>
+        <script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MSH8C97');`,
+          }}
+        ></script>
         <title>Galeri Keramik</title>
         <meta
           name="description"
