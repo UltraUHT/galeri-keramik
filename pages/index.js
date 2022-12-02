6fe5cfcf76aca954d3ddbc5af20daba815d9cf46
jeremy <jpwijanto@gmail.com>
2022-12-02 14:32:45 +0700

    replace gtag

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,9 @@ export default function Home() {
   return (
     <div>
       <Head>
+        <script>{`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MSH8C97');
+          `}</script>
         <title>Galeri Keramik</title>
         <meta
           name="description"
@@ -18,9 +21,6 @@ export default function Home() {
         />
         <link rel="icon" href="/icons/logo_tab.png" />
       </Head>
-      <script>{`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MSH8C97');
-        `}</script>
       <noscript>
         <iframe
           src="https://www.googletagmanager.com/ns.html?id=GTM-MSH8C97"
